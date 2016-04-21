a6f31d288c4dfd6cec5abac43b50898028b2cbf3
poopgiggle <poopgiggle@gmail.com>
2016-04-21 10:36:13 -0500

    add iso-snapshot-assembler script, refine print-iso-sessions script

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
 setup(name="candump-parser",
-      version="0.2",
+      version="0.3",
       packages=["vehicle_log_parser", "vehicle_log_parser/utils"],
-      scripts=["scripts/print-iso-sessions"])
+      scripts=["scripts/print-iso-sessions", "scripts/iso-snapshot-assembler"])
